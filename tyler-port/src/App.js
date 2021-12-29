799e99a08ce70e716cbd3511540232c100761e91
TylerMair <tyleramair@gmail.com>
2021-12-29 09:07:38 -0700

    about me, contact successfully displaying

--- tyler-port/src/App.js
+++ tyler-port/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+//react dependencies, importing different pages
+import React, { useState } from 'react';
+import ContactMe from './Components/Contact';
+import Nav from './Components/Nav';
+import About from './Components/About'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+        <ContactMe></ContactMe>
+      </main>
     </div>
   );
 }
--- tyler-port/src/Components/Contact/index.js
+++ tyler-port/src/Components/Contact/index.js
@@ -4,20 +4,34 @@ function ContactMe() {
 
     // Create a state for user inputs and set them to blank
     const [contactState, setContactState] = useState({ name: '', email: '', message: ''})
+    // destructure state
+    const { name, email, message } = contactState;
+
+    //create error message state and set to blank string
+    const [errorMessage, setErrorMessage ] = useState('');
+
+    const submitCheck = (e) => {
+        e.preventDefault();
+        if (!errorMessage) {
+            console.log('Submit', contactState);
+        }
+    };
+    
+    //markdown to show
     return(
         <section>
             <form id='contact-me-form'>
             <div>
                 <label htmlFor='name'>Your Name:</label>
-                <input type='text' name='name' defaultValue={name} onBlur={handleChange} />
+                <input type='text' name='name' defaultValue={name}  />
             </div>
             <div>
                 <label htmlFor='email'>Your Email:</label>
-                <input type='email' name='email' defaultValue={email} onBlur={handleChange} />
+                <input type='email' name='email' defaultValue={email}  />
             </div>
             <div>
                 <label htmlFor="message">Say Something To Me</label>
-                <textarea name='message' rows='5' defaultValue={message} onBlur={handleChange} />
+                <textarea name='message' rows='5' defaultValue={message}  />
             </div>
             <button name='button' type='submit'>Submit</button>
             </form>
