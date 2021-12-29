10c11a5e469b1b76ff7a0d9aa8238d3ecbd2b1e1
TylerMair <tyleramair@gmail.com>
2021-12-29 08:46:25 -0700

    form state

--- tyler-port/src/Components/Contact/index.js
+++ tyler-port/src/Components/Contact/index.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactMe() {
+
+    // Create a state for user inputs and set them to blank
+    const [contactState, setContactState] = useState({ name: '', email: '', message: ''})
     return(
         <section>
+            <form id='contact-me-form'>
             <div>
                 <label htmlFor='name'>Your Name:</label>
                 <input type='text' name='name' defaultValue={name} onBlur={handleChange} />
@@ -15,6 +19,10 @@ function ContactMe() {
                 <label htmlFor="message">Say Something To Me</label>
                 <textarea name='message' rows='5' defaultValue={message} onBlur={handleChange} />
             </div>
+            <button name='button' type='submit'>Submit</button>
+            </form>
         </section>
-    )
+    );
 }
+
+export default ContactMe;
