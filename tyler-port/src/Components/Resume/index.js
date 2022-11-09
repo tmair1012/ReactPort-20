796834531613e2e09605631862f03b6bbcb9717e
TylerMair <tyleramair@gmail.com>
2022-11-09 09:09:00 -0700

    changed resume picture and download

--- tyler-port/src/Components/Resume/index.js
+++ tyler-port/src/Components/Resume/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import DownloadLink from "react-download-link";
-import myResume from "../../assets/Profile/Tyler_Mair_Resume_Fixed.pdf";
-import resumeOne from "../../assets/Profile/Resume_3.png"
-import resumeTwo from "../../assets/Profile/Resume_5.png"
+import myResume from "../../assets/Profile/Tyler Mair Resume Official.pdf";
+import resumePic from "../../assets/Profile/Tyler_Mair_Official_Resume_pic.jpg"
+
+
 
 function Resume() {
   return (
     <div className="resumeDiv">
       <div className='resume-picture-div'>
-    <img src={resumeOne} className='resume-picture'></img>
-    <img src={resumeTwo} className='resume-picture'></img>
+    <img src={resumePic} className='resume-picture'></img>
     </div>
     
     
